22774d197217b893c10d5c9d2d76bd85317c0ab5
rexservarum <sog9607@hotmail.com>
2014-06-01 15:57:08 -0700

    Update MapGUI.java

--- src/MapGUI.java
+++ src/MapGUI.java
@@ -1,16 +1,19 @@
 import javax.swing.*;
-
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
 /**
  * Set main map GUIs
  */
 public class MapGUI {
     private static GameBoard board;
-    
+    private static JFrame fra;
     public MapGUI(GameBoard g){
         board = g;
     }
     public static void mapFrame(){
         JFrame frame = new JFrame();
+        fra = frame;
         frame.setSize(527,550);
         frame.setTitle( "Graphics Example" );
 
@@ -18,5 +21,18 @@ public class MapGUI {
         Map graphics = new Map(board);
         frame.add(graphics);
         frame.setVisible(true);
+        
+    }
+    
+    public void revalid()
+    {
+        fra.repaint();
+        fra.revalidate();
+
+        JPanel panel = new JPanel();
+        Map graphics = new Map(board);
+        fra.add(graphics);
+        fra.setVisible(true);
+        
     }
 }
