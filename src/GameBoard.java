eccd59546495cf6e27d4be0420209f831e231a85
Austin <austinquachh@gmail.com>
2014-06-03 11:31:25 -0700

    GameBoard: make rooms more dangerous...closer to edge

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -206,7 +206,7 @@ public class GameBoard {
             int pick = (int) ((Math.random()*2) + 1);
             if(pick == 1){
                 //Prevents from going off the screen
-                if(x>10) {
+                if(x>13) {
                     x-=2;
                 }else
                     x+= randomNum;
