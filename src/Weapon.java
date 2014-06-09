ea19430a31f99a5be31f78bd0abd0a581d8b1f56
dravianWeaver <xxx.thecounselor.xxx@gmail.com>
2014-06-08 18:02:23 -0700

    Create Weapon.java

---
+++ src/Weapon.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author DuncanSkyrim
+ */
+public class Weapon extends Gear {
+    private int attackVal;
+    private int range;
+    
+    public Weapon(Actor a, String newSlot, int atkVal, int atkRange){
+        super(a, newSlot);
+        attackVal = atkVal;
+        range = atkRange;
+    }
+    
+    public Weapon(Room a, String newSlot, int atkVal, int atkRange){
+        super(a, newSlot);
+        attackVal = atkVal;
+        range = atkRange;
+    }
+    
+    public int getAttackVal(){
+        return attackVal;
+    }
+    
+    public int getRange(){
+        return range;
+    }
+    
+    public void setAttackVal(int x){
+        attackVal = x;
+    }
+    
+    public void setRange(int x){
+        range = x;
+    }
+    
+    @Override
+    public void equipEffect(Actor a){
+        a.getAtkVal().setValue(a.getAtkVal().getValue() + process(a, attackVal));
+        a.getAtkVal().setMaxValue(a.getAtkVal().getMaxValue() + process(a, attackVal) + process(a, range));
+    }
+    @Override
+    public void unequipEffect(Actor a){
+        a.getAtkVal().setValue(a.getAtkVal().getValue() - process(a, attackVal));
+        a.getAtkVal().setMaxValue(a.getAtkVal().getMaxValue() -  process(a, attackVal) - process(a, range));
+    }
+    
+    public int process(Actor a, int x){
+        System.out.println((double) a.getStr().getValue()/10 + " strength modifier");
+        return (int) (x * (1 + (double) a.getStr().getValue()/10 ));     
+    }
+}
