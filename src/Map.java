329a34a511db19a3e1c4f63f29ca897724978cce
rexservarum <sog9607@hotmail.com>
2014-06-11 11:54:03 -0700

    Update Map.java

--- src/Map.java
+++ src/Map.java
@@ -1,60 +1,69 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import sun.font.GraphicComponent;
 
 /**
  * Background frameworks for the map.
  */
 public class Map extends JComponent {
     private static GameBoard board;
+    private static JPanel mainPanel = new JPanel();
     
     public Map (GameBoard g){
         board = g;
     }
-    public void paintComponent(Graphics g){
-        // Recover Graphics g
-        Graphics2D g2 = (Graphics2D) g;
-        int[][] map = new int[17][17];
-
-        int column = 0, row;
-        for( int r = 0; r < map.length; r++ ){
-            for( int c = 0; c < map[0].length; c++ ){
-                row = r*30;
-                column = c*30;
-                g2.setColor( Color.BLACK);
-                Rectangle2D.Double rectangle1 = new Rectangle.Double( row, column, 30, 30 );
-                g2.draw( rectangle1 );
-
-                //g2.drawString( "A", (int)(Math.random()+1)*row + 5, (int)(Math.random()+1)*column -5);
-            }
+    public void paintComponent(Graphics g)
+    {
+        // http://stackoverflow.com/questions/18777893/jframe-background-image
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new File("space.jpg"));
+        } catch (IOException ex) {
+            Logger.getLogger(GraphicComponent.class.getName()).log(Level.SEVERE, null, ex);
         }
-
+        super.paintComponent(g);
+        g.drawImage(img, 0, 0, this);
+        
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.BLACK);
+        int[][] map = new int[17][17];
+        
         int sRow = 0;
         int sColumn = 0;
         for( int r = 0; r < map.length; r++ ){
             for(int c = 0; c < map[0].length; c++ ){
                 sRow = r * 30;
                 sColumn = (map[0].length- c - 1)*30;
-                //System.out.print(random);
                 Room testRoom = board.getLocation(r,c, "0");
                 if( testRoom != null){
                     if(board.getPlayer().getLocation().equals(testRoom))
                     {
+                        g2.setColor( Color.WHITE );
+                        g2.fillRect( sRow, sColumn, 30,30);
                         g2.setColor( Color.RED );
                         g2.drawRect( sRow, sColumn, 30,30);
                         g2.setColor( Color.BLACK );
                         g2.drawString("P", sRow + 12, sColumn +20);
                     }
                     else if(testRoom.hasExplored()){
+                        g2.setColor( Color.WHITE );
+                        g2.fillRect( sRow, sColumn, 30,30);
+                        g2.setColor( Color.BLACK );
+                        g2.drawRect( sRow, sColumn, 30, 30 );
                         g2.drawString(testRoom.getSymbol(), sRow + 2, sColumn +20);
                     }
                     else
                     {
+                        g2.setColor(Color.BLACK);
                         g2.fillRect(sRow, sColumn, 30, 30);
                         g2.setColor(Color.BLUE);
                         g2.drawRect(sRow, sColumn, 30, 30);
-                        g2.setColor(Color.BLACK);
-                        //g2.drawString("0", sRow + 12, sColumn - 10);
                     }
                 }
             }
