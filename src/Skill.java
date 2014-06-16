282d1edef37b9714e546edbdf4187cee82f2d42d
dravianWeaver <xxx.thecounselor.xxx@gmail.com>
2014-06-15 20:35:22 -0700

    Update Skill.java

--- src/Skill.java
+++ src/Skill.java
@@ -1,19 +1,27 @@
 import java.lang.*;
 import java.util.*;
+import javax.swing.JTextArea;
 
 public class Skill {
     private Skill opposingSkill;
     private Actor user;
     private String name;
+    private JTextArea mainField;
     
     public Skill(Actor a){
         a.getKnownSkills().add(this);
         user = a;
         name = "Generic Skill 1012";
+        mainField = user.getLocation().getGameBoard().getMainGame().getTextArea();
     }
     
     public Skill (String s){
         
+        
+    }
+    
+    public JTextArea getMainField(){
+        return mainField;
     }
     
     public String getName(){
